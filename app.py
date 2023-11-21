7eb0de756b6f5fea78f68b2f9f2e2f7d291c5e90
Kevin-Lad <kevinladisheff@gmail.com>
2023-11-20 18:13:10 -0700

    app updated

--- app.py
+++ app.py
@@ -48,95 +48,10 @@ def data():
 def stroke():
     return render_template("heart_stroke_prediction.html")
 
-# ---------------------------------------------------------
-# Machine Learning inputs
 
 
-# ---------------------------------------------------------
-# API
-# ---------------------------------------------------------
-
-# ---------------------------------------------------------
-# Datatable
-@app.route("/api/movies")
-def movie_grid():
-
-    session = Session(engine)
-
-    results = session.query(Movies.title, Movies.director, Movies.year, Movies.rating, Movies.imdb_votes, Movies.imdb_score).all()
-
-    results = [list(r) for r in results]
-
-    table_results = {
-        "table": results
-    }
-
-    session.close()
-
-    return jsonify(table_results)
-
-# -------------------------------------------------------------------------------
-# Charts
-@app.route("/api/years/<year>")
-def years(year):
-
-    session = Session(engine)
-
-    if year == "before":
-        results = session.query(Movies.title, Movies.director, Movies.year, 
-            Movies.rating, Movies.imdb_votes, Movies.imdb_score).filter(Movies.year < 2000).all()
-    else:
-        results = session.query(Movies.title, Movies.director, Movies.year, 
-            Movies.rating, Movies.imdb_votes, Movies.imdb_score).filter(Movies.year >= 2000).all()
-
-    results = [list(r) for r in results]
-
-    rating = [result[3] for result in results]
-    votes = [result[4] for result in results]
-
-    year_results = {
-        "rating": rating,
-        "votes": votes,
-    }
-
-    session.close()
-
-    return jsonify(year_results)
 
 
-@app.route("/api/directors/<director>")
-def directors(director):
-
-    print(director)
-
-    if director == "chaplin":
-        name = "Charles Chaplin"
-    elif director == "hitchcock":
-        name = "Alfred Hitchcock"
-    elif director == "nolan":
-        name = "Christopher Nolan"
-    else:
-        name = "Akira Kurosawa"
-
-    session = Session(engine)
-
-    G_results = session.query(func.avg(Movies.imdb_score)).filter(Movies.rating=="G").filter(Movies.director == name).all()
-    PG_results = session.query(func.avg(Movies.imdb_score)).filter(Movies.rating=="PG").filter(Movies.director == name).all()
-    PG_plus_results = session.query(func.avg(Movies.imdb_score)).filter(or_(Movies.rating=="PG+", Movies.rating=="PG-13")).filter(Movies.director == name).all()
-    R_results = session.query(func.avg(Movies.imdb_score)).filter(Movies.rating=="R").filter(Movies.director == name).all()
-    Other_results = session.query(func.avg(Movies.imdb_score)).filter(or_(Movies.rating=="APPROVED",Movies.rating=="NOT RATED", Movies.rating=="N\A", Movies.rating=="PASSED")).filter(Movies.director == name).all()
-
-    results = [G_results[0][0], PG_results[0][0], PG_plus_results[0][0], R_results[0][0], Other_results[0][0]]
-    labels = ["G", "PG", "PG+", "R", "Other"]
-
-    director_results = {
-        "labels": labels,
-        "scores": results,
-    }
-
-    session.close()
-
-    return jsonify(director_results)
 
 @app.route("/model", methods=["POST"])
 def predict_heart_stroke():
